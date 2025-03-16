2ddc240a9117511434a25e66156e2f2cef41f787
Miche Mayer <miche.mayer@outlook.de>
2025-03-16 20:49:24 +0000

    remove size contract from theme

--- src/utils/theme.utils.ts
+++ src/utils/theme.utils.ts
@@ -1,22 +1,18 @@
 import { createTheme as createVanillaExtractTheme } from '@vanilla-extract/css'
 import { colorContract, type ColorContract } from '@contracts/color.contract.css'
 import { typographyContract, type TypographyContract } from '@contracts/typography.contract.css'
-import { fontSizeContract, type FontSizeContract } from '@contracts/font-size.contract.css'
 
 export const createTheme = (
     colorProperties: ColorContract,
     typographyProperties: TypographyContract,
-    fontSizeProperties: FontSizeContract,
 ) =>
 createVanillaExtractTheme(
     {
         colors: colorContract,
         typography: typographyContract,
-        fontsize: fontSizeContract,
     },
     {
         colors: colorProperties,
         typography: typographyProperties,
-        fontsize: fontSizeProperties,
     }
 )
--- types/utils/theme.utils.d.ts
+++ types/utils/theme.utils.d.ts
@@ -1,4 +1,3 @@
 import { type ColorContract } from '../contracts/color.contract.css';
 import { type TypographyContract } from '../contracts/typography.contract.css';
-import { type FontSizeContract } from '../contracts/font-size.contract.css';
-export declare const createTheme: (colorProperties: ColorContract, typographyProperties: TypographyContract, fontSizeProperties: FontSizeContract) => string;
+export declare const createTheme: (colorProperties: ColorContract, typographyProperties: TypographyContract) => string;
